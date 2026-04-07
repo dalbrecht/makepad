476f37286f5621b3a8c15ddf4fb4820a279a9f68
Don Albrecht <dalbrecht@dademmedia.com>
2026-04-07 13:34:28 -0400

    Revert "fix(script): replace HashMap with BTreeMap in ValueMap to fix WASM codegen bug"
    This reverts commit 440b7678e1355381d9e94b81ba93b7caab5f5aca.

--- platform/script/src/value_map.rs
+++ platform/script/src/value_map.rs
@@ -1,39 +1,90 @@
 use crate::makepad_live_id::LiveId;
 
 use std::{
-    collections::BTreeMap,
+    collections::HashMap,
     ops::{Deref, DerefMut, Index, IndexMut},
 };
 
-// Previously used HashMap with a custom zero-cost hasher (ValueHasher).
-// Replaced with BTreeMap to work around an LLVM wasm32 codegen bug that
-// miscompiles HashMap::insert, silently dropping keys during Makepad's
-// DSL evaluation. BTreeMap uses comparison (Ord) instead of hashing,
-// avoiding the buggy code path entirely. Performance is equivalent for
-// the small maps used here (typically 3-57 entries).
-// See: https://github.com/dalbrecht/makepad/issues/6
+// Idea taken from the `nohash_hasher` crate.
+#[derive(Default)]
+pub struct ValueHasher(u64);
+
+impl std::hash::Hasher for ValueHasher {
+    fn write(&mut self, _: &[u8]) {
+        unreachable!();
+    }
+    fn write_u8(&mut self, _n: u8) {
+        unreachable!();
+    }
+    fn write_u16(&mut self, _n: u16) {
+        unreachable!();
+    }
+    fn write_u32(&mut self, _n: u32) {
+        unreachable!();
+    }
+    #[inline(always)]
+    fn write_u64(&mut self, n: u64) {
+        self.0 = n;
+    }
+    fn write_usize(&mut self, _n: usize) {
+        unreachable!();
+    }
+    fn write_i8(&mut self, _n: i8) {
+        unreachable!();
+    }
+    fn write_i16(&mut self, _n: i16) {
+        unreachable!();
+    }
+    fn write_i32(&mut self, _n: i32) {
+        unreachable!();
+    }
+    fn write_i64(&mut self, _n: i64) {
+        unreachable!();
+    }
+    fn write_isize(&mut self, _n: isize) {
+        unreachable!();
+    }
+    #[inline(always)]
+    fn finish(&self) -> u64 {
+        self.0
+    }
+}
+
+#[derive(Copy, Clone, Default)]
+pub struct ValueHasherBuilder {}
+
+impl std::hash::BuildHasher for ValueHasherBuilder {
+    type Hasher = ValueHasher;
+
+    #[inline(always)]
+    fn build_hasher(&self) -> ValueHasher {
+        ValueHasher::default()
+    }
+}
 
 #[derive(Clone, Debug)]
 pub struct ValueMap<K, V> {
-    map: BTreeMap<K, V>,
+    map: HashMap<K, V, ValueHasherBuilder>,
+    //alloc_set: HashSet<K, LiveIdHasherBuilder>
 }
 
 impl<K, V> Default for ValueMap<K, V>
 where
-    K: Ord + Copy + From<LiveId> + std::fmt::Debug,
+    K: std::cmp::Eq + std::hash::Hash + Copy + From<LiveId> + std::fmt::Debug,
 {
     fn default() -> Self {
         Self {
-            map: BTreeMap::new(),
+            map: HashMap::with_hasher(ValueHasherBuilder {}),
+            //alloc_set: HashSet::with_hasher(LiveIdHasherBuilder {})
         }
     }
 }
 
 impl<K, V> Deref for ValueMap<K, V>
 where
-    K: Ord + Copy + From<LiveId>,
+    K: std::cmp::Eq + std::hash::Hash + Copy + From<LiveId>,
 {
-    type Target = BTreeMap<K, V>;
+    type Target = HashMap<K, V, ValueHasherBuilder>;
     fn deref(&self) -> &Self::Target {
         &self.map
     }
@@ -41,7 +92,7 @@ where
 
 impl<K, V> DerefMut for ValueMap<K, V>
 where
-    K: Ord + Copy + From<LiveId>,
+    K: std::cmp::Eq + std::hash::Hash + Copy + From<LiveId>,
 {
     fn deref_mut(&mut self) -> &mut Self::Target {
         &mut self.map
@@ -50,7 +101,7 @@ where
 
 impl<K, V> Index<K> for ValueMap<K, V>
 where
-    K: Ord + Copy + From<LiveId>,
+    K: std::cmp::Eq + std::hash::Hash + Copy + From<LiveId>,
 {
     type Output = V;
     fn index(&self, index: K) -> &Self::Output {
@@ -60,7 +111,7 @@ where
 
 impl<K, V> IndexMut<K> for ValueMap<K, V>
 where
-    K: Ord + Copy + From<LiveId>,
+    K: std::cmp::Eq + std::hash::Hash + Copy + From<LiveId>,
 {
     fn index_mut(&mut self, index: K) -> &mut Self::Output {
         self.map.get_mut(&index).unwrap()
